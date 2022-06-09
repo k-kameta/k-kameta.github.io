0f70c60f49d985681e9466de096fb464979fc62e
KK <91944125+k-kameta@users.noreply.github.com>
2022-06-09 14:49:41 +0900

    update

---
+++ js/dates.js
@@ -0,0 +1,8 @@
+
+
+const lastmod = document.querySelector('#lastmod');
+lastmod.innerHTML = `Last updated: ${document.lastModified}`;
+
+let text = document.lastModified;
+document.getElementById("mod-date").innerHTML = text;
+
