4fe1fb168263f0a90a47acadecb6dcdc97aecd75
KK <91944125+k-kameta@users.noreply.github.com>
2022-07-03 04:19:25 +0900

    u

--- weather/js/scripts.js
+++ weather/js/scripts.js
@@ -13,3 +13,15 @@ window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('
 
   classList property - https://developer.mozilla.org/en-US/docs/Web/API/Element/classList
   */
+
+
+function adjustRating(rating) {
+    document.getElementById("ratingvalue").innerHTML = rating;
+}
+
+function selectResponse() {
+	const s = document.querySelector('#selected');
+	const sel = document.querySelector('#selectbrowser');
+	s.style.display = "block";
+	s.textContent = sel.value;
+}
