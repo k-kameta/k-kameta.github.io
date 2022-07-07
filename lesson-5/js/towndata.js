437bb309bd645b4af20ef55ca894a2efbc8eb2e3
KK <91944125+k-kameta@users.noreply.github.com>
2022-07-07 18:59:18 +0900

    u

--- lesson-5/js/towndata.js
+++ lesson-5/js/towndata.js
@@ -36,49 +36,49 @@ fetch(requestURL)
                       towns[i].name == "Soda Springs" || 
                       towns[i].name == "Fish Haven") {
 
-            let card = document.createElement('section');
-            let div1 = document.createElement('div');
-            let div2 = document.createElement('div');
+                let card = document.createElement('section');
+                let div1 = document.createElement('div');
+                let div2 = document.createElement('div');
 
-            let h2 = document.createElement('h2');
-            let motto = document.createElement('motto');
-            let yearFounded = document.createElement('yearFounded');
-            let currentPopulation = document.createElement('currentPopulation');
-            let averageRainfall = document.createElement('averageRainfall');
-            let photo = document.createElement('img');
+                let h2 = document.createElement('h2');
+                let motto = document.createElement('motto');
+                let yearFounded = document.createElement('yearFounded');
+                let currentPopulation = document.createElement('currentPopulation');
+                let averageRainfall = document.createElement('averageRainfall');
+                let photo = document.createElement('img');
 
-            h2.textContent = towns[i].name;
-            motto.textContent = `Motto: ${towns[i].motto}`;
-			yearFounded.textContent = `Year Founded: ${towns[i].yearFounded}`;
-			currentPopulation.textContent = `Population: ${towns[i].currentPopulation}`;
-			averageRainfall.textContent = `Annual Rainfall: ${towns[i].averageRainfall}`;
-            photo.setAttribute("alt", `Image of ${towns[i].name}`);
-            // photo.setAttribute('src', towns[i].imageurl);
-            if (towns[i].name == "Preston") {
-                photo.setAttribute('src', "images/wdd230_image_franklinOriginal.jpeg");
-            }
-            else if (towns[i].name == "Soda Springs") {
-                photo.setAttribute('src', "images/soda-springs-gc4fef8b4b_640.jpg");
-            }
-            else if (towns[i].name == "Fish Haven") {
-                photo.setAttribute('src', "images/FishHaven-dreamstime_s_85958730.jpg");
-            }
-
-
-            // photo.setAttribute('src', "images/wdd230_image_franklinOriginal.jpeg");
+                h2.textContent = towns[i].name;
+                motto.textContent = `Motto: ${towns[i].motto}`;
+                yearFounded.textContent = `Year Founded: ${towns[i].yearFounded}`;
+                currentPopulation.textContent = `Population: ${towns[i].currentPopulation}`;
+                averageRainfall.textContent = `Annual Rainfall: ${towns[i].averageRainfall}`;
+                photo.setAttribute("alt", `Image of ${towns[i].name}`);
+                // photo.setAttribute('src', towns[i].imageurl);
+                if (towns[i].name == "Preston") {
+                    photo.setAttribute('src', "images/wdd230_image_franklinOriginal.jpeg");
+                }
+                else if (towns[i].name == "Soda Springs") {
+                    photo.setAttribute('src', "images/soda-springs-gc4fef8b4b_640.jpg");
+                }
+                else if (towns[i].name == "Fish Haven") {
+                    photo.setAttribute('src', "images/FishHaven-dreamstime_s_85958730.jpg");
+                };
+                
+                div1.classList.add("townInfo");
+                div1.appendChild(h2);
+                div1.appendChild(motto);
+                div1.appendChild(yearFounded);
+                div1.appendChild(currentPopulation);
+                div1.appendChild(averageRainfall);
             
-            div1.appendChild(h2);
-            div1.appendChild(motto);
-            div1.appendChild(yearFounded);
-            div1.appendChild(currentPopulation);
-            div1.appendChild(averageRainfall);
-            div2.appendChild(photo);
-
-            card.appendChild(div1);
-            card.appendChild(div2);
-            document.querySelector('div.cards').appendChild(card);
+                div2.classList.add("townPhoto");
+                div2.appendChild(photo);
+        
+                card.appendChild(div1);
+                card.appendChild(div2);
+                document.querySelector('div.cards').appendChild(card);
                         
-            }
+                }
 
         }
     });
